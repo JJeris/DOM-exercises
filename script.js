d7df3a5ac69bacf7961d2268d099f2834e25089b
Johans Justs Eris <johans.eris@r6vsk.lv>
2022-11-19 21:08:21 +0200

    Add examples

--- script.js
+++ script.js
@@ -36,3 +36,33 @@ const meToo = document.createElement('p');
 meToo.textContent = "ME TOO!";
 
 blackDiv.appendChild(meToo);
+
+// // the JavaScript file
+// const btn = document.querySelector('#btn');
+// btn.onclick = () => alert("Hello World");
+// the JavaScript file
+const btn = document.querySelector('#btn');
+// btn.addEventListener('click', () => {
+//   alert("Hello World");
+// });
+
+// btn.addEventListener('click', function (e) {
+//     console.log(e);
+//   });
+
+
+btn.addEventListener('click', function (e) {
+  e.target.style.background = 'blue';
+});
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    alert(button.id);
+  });
+});
